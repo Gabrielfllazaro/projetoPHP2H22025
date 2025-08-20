8076de76487e10113feb7da260b2f0ebfe6918ad
Gabrielfllazaro <felipelemelazaro@gmail.com>
2025-08-19 22:15:53 -0300

    trabalhando com estruturas de operações

--- operadores.php
+++ operadores.php
@@ -8,4 +8,22 @@
 
 	echo("Insira a operação desejada: <br />");
 
+	$num1 = 10;
+	$num2 = 2;
+
+	$resp1 = $num1 + $num2;
+	$resp2 = $num1 - $num2;
+	$resp3 = $num1 * $num2;
+	$resp4 = $num1 / $num2;
+	$resp5 = $num1 % $num2;
+
+	echo("A soma de $num1 e $num2 é : $resp1 <br/>");
+
+	echo("A subtração de $num1 e $num2 é : $resp2 <br/>");
+
+	echo("A multiplicação de $num1 e $num2 é : $resp3 <br/>");
+
+	echo("A divisão de $num1 e $num2 é : $resp4 <br/>");
+
+	echo("A resto da divisão de $num1 e $num2 é : $resp5 <br/>");
  ?>
